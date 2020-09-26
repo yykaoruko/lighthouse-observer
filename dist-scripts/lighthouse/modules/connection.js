d711e929666e05fafb70eec4d1142bab0a72a7fc
Kaoruko <yykaoruko@gmail.com>
2020-09-26 22:59:05 +0900

    fix: query

--- dist-scripts/lighthouse/modules/connection.js
+++ dist-scripts/lighthouse/modules/connection.js
@@ -8,7 +8,7 @@ exports.storeLighthouseResult = void 0;
 const storeLighthouseResult = (client, lighthouseResult) => {
   const insertQuery = 'INSERT INTO lighthouse SET ?';
   client.connect();
-  client.query(insertQuery, lighthouseResult, (err, res) => {
+  client.query(insertQuery, [lighthouseResult], (err, res) => {
     if (err) throw err;
 
     for (let row of res.rows) {
--- scripts/lighthouse/modules/connection.js
+++ scripts/lighthouse/modules/connection.js
@@ -1,7 +1,7 @@
 export const storeLighthouseResult = (client, lighthouseResult) => {
   const insertQuery = 'INSERT INTO lighthouse SET ?';
   client.connect();
-  client.query(insertQuery, lighthouseResult, (err, res) => {
+  client.query(insertQuery, [lighthouseResult], (err, res) => {
     if (err) throw err;
     for (let row of res.rows) {
       console.log(JSON.stringify(row));
